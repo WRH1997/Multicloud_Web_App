226a3b889844b17b611ac8d15a85be5cac4246ee
Adithya Hadadi <ad368540@dal.ca>
2023-06-07 11:49:47 -0300

    added a lambda function that serves as the DynamoDB client

--- trivia-titans-frontend/src/components/common/DynamoDBCRUDClient.jsx
+++ trivia-titans-frontend/src/components/common/DynamoDBCRUDClient.jsx
@@ -1,87 +1,85 @@
-const AWS = require('aws-sdk');
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocument } = require("@aws-sdk/lib-dynamodb");
 
-class DynamoDBCRUDClient{
-  constructor(tableName) {
-    this.tableName = tableName;
-    this.dynamoDB = new AWS.DynamoDB.DocumentClient();
-  }
+const dbClient = new DynamoDBClient({ region: "us-east-1" });
+const ddbDocClient = DynamoDBDocument.from(dbClient);
 
-  async createItem(item) {
-    const params = {
-      TableName: this.tableName,
-      Item: item,
-    };
 
-    try {
-      await this.dynamoDB.put(params).promise();
-      console.log('Item created successfully');
-      return true;
-    } catch (error) {
-      console.error('Error creating item:', error);
-      return false;
-    }
-  }
-
-  async getItem(key) {
-    const params = {
-      TableName: this.tableName,
-      Key: key,
-    };
-
-    try {
-      const result = await this.dynamoDB.get(params).promise();
-      const item = result.Item;
-      if (item) {
-        console.log('Item retrieved successfully:', item);
-      } else {
-        console.log('Item not found');
-      }
-      return item;
-    } catch (error) {
-      console.error('Error retrieving item:', error);
-    }
-
-  }
-/* example:
-const updateExpression = 'SET age = :newAge';
-const expressionAttributeValues = {
-  ':newAge': 31,
-};
- */
-  async updateItem(key, updateExpression, expressionAttributeValues) {
-    const params = {
-      TableName: this.tableName,
-      Key: key,
-      UpdateExpression: updateExpression,
-      ExpressionAttributeValues: expressionAttributeValues,
-      ReturnValues: 'ALL_NEW',
-    };
-
-    try {
-      const result = await this.dynamoDB.update(params).promise();
-      const updatedItem = result.Attributes;
-      console.log('Item updated successfully:', updatedItem);
-      return result;
-    } catch (error) {
-      console.error('Error updating item:', error);
-    }
-  }
-
-  async deleteItem(key) {
-    const params = {
-      TableName: this.tableName,
-      Key: key,
-    };
-
-    try {
-      await this.dynamoDB.delete(params).promise();
-      console.log('Item deleted successfully');
-      return true;
-    } catch (error) {
-      console.error('Error deleting item:', error);
-      return false;
-    }
+/* Example of json to lambda :
+{
+  "tableName":"<<TABLE-NAME>>",
+  "operation": "UPDATE",
+  "key": {
+    "id": "1"
+  },
+  "updateExpression": "set #name = :name, description = :description",
+  "expressionAttributeValues": {
+    ":name": "Updated Item 1",
+    ":description": "This is updated item 1"
   }
 }
+ */
 
-module.exports = DynamoDBCRUDClient;
+exports.handler = async (event) => {
+  const tableName = event.tableName;
+  const operation =event.operation.toUpperCase();
+  let response;
+  switch (operation) {
+    case 'CREATE':
+      response = await createItem(event.item);
+      break;
+    case 'READ':
+      response = await readItem(event.key);
+      break;
+    case 'UPDATE':
+      response = await updateItem(event.key, event.updateExpression, event.expressionAttributeValues);
+      break;
+    case 'DELETE':
+      response = await deleteItem(event.key);
+      break;
+    default:
+      response = `Invalid operation: ${event.operation}`;
+      break;
+  }
+  return response;
+};
+
+async function createItem(item) {
+  const params = {
+    TableName: 'your-table-name',
+    Item: item,
+  };
+
+  return ddbDocClient.put(params);
+}
+
+async function readItem(key) {
+  const params = {
+    TableName: 'your-table-name',
+    Key: key,
+  };
+
+  const { Item } = await ddbDocClient.get(params);
+  return Item;
+}
+
+async function updateItem(key, updateExpression, expressionAttributeValues) {
+  const params = {
+    TableName: 'your-table-name',
+    Key: key,
+    UpdateExpression: updateExpression,
+    ExpressionAttributeValues: expressionAttributeValues,
+    ReturnValues:"UPDATED_NEW"
+  };
+
+  return ddbDocClient.update(params);
+}
+
+async function deleteItem(key) {
+  const params = {
+    TableName: 'your-table-name',
+    Key: key,
+  };
+
+  return ddbDocClient.delete(params);
+}
