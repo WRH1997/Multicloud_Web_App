04ef8deb657149d5784828b47b6661fc014e87f5
Waleed Alhindi <wl392785@dal.ca>
2023-06-08 09:35:31 -0300

    Implemented Dynamo Edit User

--- trivia-titans-frontend/src/components/userProfile/EditProfile.jsx
+++ trivia-titans-frontend/src/components/userProfile/EditProfile.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {useEffect, useState} from 'react';
+import {useEffect, useState, useMemo} from 'react';
 import '../../themes/EditProfile.css'
 const AWS = require("aws-sdk")
 
 const EditProfile = () => {
+
 
     const [userData, setUserData] = useState({
         firstName:"",
@@ -15,6 +16,8 @@ const EditProfile = () => {
         phone: 1
     });
 
+
+    //DUMMY DATA FOR TESTING
     let currFname = "DUMMY TEXT";
     let currLname = "DUMMY TEXT";
     let currUname = "DUMMY TEXT";
@@ -23,10 +26,10 @@ const EditProfile = () => {
     let currEmail = "DUMMY TEXT";
     let currPhone = 1;
 
-    AWS.config = new AWS.Config();
-    AWS.config.accessKeyId = "";
-    AWS.config.secretAccessKey = "";
-    AWS.config.region = "";
+    AWS.config.update({
+        credentials: new AWS.Credentials('AKIAR2VYCU2GQKVNVKGF', 'aOQlTUbsnXQwFqVscaIE+7gmd3dAaI8tN2HSvwl8'),
+        region: 'us-east-1'
+    });
 
     const dynamoClient = new AWS.DynamoDB.DocumentClient({});
 
@@ -34,8 +37,8 @@ const EditProfile = () => {
         await dynamoClient.get({
             TableName: "dt1",
             Key: {
-                username: "user2",
-                email: "ex2"
+                username: "user1",
+                email: "ex@ex.com"
             }
         }).promise().then((data) => {
             currFname = data.Item.fname;
@@ -46,17 +49,38 @@ const EditProfile = () => {
             currPwd = data.Item.password;
         }).catch(console.error);
     }
+    //getUserInfo();
 
 
-    const onChange = (event) => {
+    const onChange = async (event) => {
+        event.preventDefault();
         let stateName = event.target.name;
         let stateVal = event.target.value;
-        setUserData(values=>({...values, [stateName]:stateVal}))
+        await setUserData(values=>({...values, [stateName]:stateVal}));
+        //console.log(userData);
     }
 
-    const submit = (event) => {
-        //UPDATE DYNAMO DB
-        alert(JSON.stringify(userData));
+    const submit = async (event) => {
+        event.preventDefault();
+        let updateParams = {
+            TableName: "dt1",
+            Key: {
+                username: "user1",
+                email: "ex"
+            },
+            //NOTE EMAIL AND USERNAME MISSING BECAUSE UNEDITABLE
+            UpdateExpression: "set firstName = :firstName, lastName = :lastName, password = :password, profilePic = :profilePic, phone = :phone",
+            ExpressionAttributeValues:{
+                ":firstName": userData.firstName,
+                ":lastName": userData.lastName,
+                ":password": userData.password,
+                ":profilePic": userData.profilePic,
+                ":phone": userData.phone
+            }
+        }
+        alert(updateParams);
+        console.log(updateParams);
+        await dynamoClient.update(updateParams).promise();
     }
 
 
@@ -65,11 +89,11 @@ const EditProfile = () => {
             <div class='form-div'>
                 <h3>Edit User Profile</h3>
                 <form onSubmit={submit}>
-                    <label htmlFor='fname' className='form-lbl'>First name: </label>
-                    <input type='text' className='fname' name='fname' onChange={onChange} placeholder={currFname}></input>
+                    <label htmlFor='firstName' className='form-lbl'>First name: </label>
+                    <input type='text' name='firstName' className='firstName' onChange={onChange} disbaled={'true'} placeholder={currFname}></input>
                     <br></br><br></br>
-                    <label htmlFor='lname' className='form-lbl'>Last name: </label>
-                    <input type='text' name='lname' className='lname' onChange={onChange} placeholder={currLname}></input>
+                    <label htmlFor='lastName' className='form-lbl'>Last name: </label>
+                    <input type='text' name='lastName' className='lastName' onChange={onChange} placeholder={currLname}></input>
                     <br></br><br></br>
                     <label htmlFor='username' className='form-lbl'>Username: </label>
                     <input type='text' name='username' className='username' onChange={onChange} placeholder={currUname}></input>
@@ -77,11 +101,11 @@ const EditProfile = () => {
                     <label htmlFor='password' className='form-lbl'>Password: </label>
                     <input type='password' name='password' className='password' onChange={onChange} placeholder={currPwd}></input>
                     <br></br><br></br>
-                    <label htmlFor='profPic' className='form-lbl'>Profile Pic URL: </label>
-                    <input type='text' name='profPic' className='profPic' onChange={onChange} placeholder={currProfPic}></input>
+                    <label htmlFor='profilePic' className='form-lbl'>Profile Pic URL: </label>
+                    <input type='text' name='profilePic' className='profilePic' onChange={onChange} placeholder={currProfPic}></input>
                     <br></br><br></br>
                     <label htmlFor='email' className='form-lbl'>Email Address: </label>
-                    <input type='text' name='email' className='email' onChange={onChange} placeholder={currEmail}></input>
+                    <input type='text' name='email' className='email' onChange={onChange} disbaled={'true'} placeholder={currEmail}></input>
                     <br></br><br></br>
                     <label htmlFor='phone' className='form-lbl'>Phone Number: </label>
                     <input type='text' name='phone' className='phone' onChange={onChange} placeholder={currPhone}></input>
