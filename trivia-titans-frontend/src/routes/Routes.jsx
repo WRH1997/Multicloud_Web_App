a6726c4c1c08911ccbb60efc4143fe3325186990
Rishi Vasa <rs310602@dal.ca>
2023-06-28 05:33:49 -0300

    Squashed commit of the following:
    commit 3066ccd5f667260fefc45e6907da04164c1ade27
    Merge: c04bd97 2d380c3
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Sat Jun 24 18:40:23 2023 -0300
    
        Merge branch 'develop-waleed' into 'develop'
    
        Merge Develop-Waleed into Develop
    
        See merge request vasa/csci5410-sdp-10!10
    
    commit 2d380c3b761385172eaa44b48af679bae406bb96
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Sat Jun 24 18:38:08 2023 -0300
    
        Integrated Cognito
    
    commit 1346ac6ce7bd8e6eb81ac929213fdf797f0ef03f
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Sat Jun 24 15:27:51 2023 -0300
    
        Finalized Edit Profile
    
    commit c04bd972b1d5bf4bfa2fe80520fbadce19b29d34
    Merge: 5430413 36f7314
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Sat Jun 24 15:26:03 2023 -0300
    
        Merge branch 'develop-waleed' into 'develop'
    
        Merge Develop-Waleed into Develop Branch
    
        See merge request vasa/csci5410-sdp-10!9
    
    commit 36f73141aa319ba4c98f691813846505bec2fbf8
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Sat Jun 24 15:24:02 2023 -0300
    
        Finalized Edit Profile
    
    commit 5430413189d3293efd6366dd5b64b2ebc8bb9cd4
    Merge: 0588e7f 0c15abc
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Sat Jun 24 15:20:19 2023 -0300
    
        Merge branch 'develop-waleed' into 'develop'
    
        Merge Develop-Waleed into Develop Branch
    
        See merge request vasa/csci5410-sdp-10!8
    
    commit 0c15abcf0077eb71616faf1789da2eb9d757b9be
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Sat Jun 24 15:17:15 2023 -0300
    
        Finalized Edit Profile
    
    commit e682df60d7b9752d3bc7871c9bb723cf134293bf
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Sat Jun 24 15:16:37 2023 -0300
    
        Finalized Edit Profile
    
    commit 0588e7f93259450b4d85e1d1973a8a0773d976ed
    Merge: 64b5381 ae6b5c1
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Tue Jun 20 09:41:17 2023 -0300
    
        Merge branch 'develop-waleed' into 'develop'
    
        Merging "Edit Profile" Feature into Development Branch
    
        See merge request vasa/csci5410-sdp-10!7
    
    commit ae6b5c117370b15da6f37837f18ceb3e679082d5
    Merge: 89e6748 64b5381
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Tue Jun 20 09:38:55 2023 -0300
    
        Finalized Edit Profile
    
    commit 89e67481fa5a486ca9f4d0ffeae6bb704ec58203
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Tue Jun 20 08:19:10 2023 -0300
    
        Resolving Issues
    
    commit 3fb40cefaeb13505c470b8a502b1dae3e1db80a4
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Thu Jun 8 15:49:46 2023 -0300
    
        Implemented Dynamo Edit User
    
    commit 04ef8deb657149d5784828b47b6661fc014e87f5
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Thu Jun 8 09:35:31 2023 -0300
    
        Implemented Dynamo Edit User
    
    commit 9be958bfb49009a97ebcab6911b59d38466f3317
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Wed Jun 7 18:44:33 2023 -0300
    
        Initial Implementation
    
    commit f8ce149c64ba79d5cab899f4c9f5039fbdd83f60
    Author: Waleed Alhindi <wl392785@dal.ca>
    Date:   Wed Jun 7 16:27:42 2023 -0300
    
        Pre-AWS Skeleton Form

--- trivia-titans-frontend/src/routes/Routes.jsx
+++ trivia-titans-frontend/src/routes/Routes.jsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Route, Routes } from 'react-router-dom';
 import LandingPage from '../components/common/LandingPage';
 import DefaultNotFound from '../components/common/DefaultNotFound';
-import CreateTriviaGame from "../components/admin/ConfigureTriviaGameConfigurations/CreateTriviaGame";
+import CreateTriviaGame from "../components/admin/CreateTriviaGame";
+import EditProfile from "../components/userProfile/EditProfile"
 import HandleSignUp from "../components/login/Signup";
 import Logout from "../components/login/Logout";
 import Login from "../components/login/Login";
@@ -16,6 +17,7 @@ const AppRoutes = () => {
             <Route path="/" element={<LandingPage />} />
             <Route path='*' element={<DefaultNotFound />} />
             <Route path='CreateTriviaGame' element={<CreateTriviaGame />} />
+            <Route path='/EditProfile' element={<EditProfile />} />
             <Route path='SignUp' element={<HandleSignUp/>} />
             <Route path='Login' element={<Login/>} />
             <Route path='Logout' element={<Logout/>} />
