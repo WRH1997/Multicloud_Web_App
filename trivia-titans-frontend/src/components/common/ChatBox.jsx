8131495ceabe066a67d30b83b07e042c68839ac8
Adithya Hadadi <ad368540@dal.ca>
2023-07-10 17:17:12 -0300

    a basic implementation of real-time chat

--- trivia-titans-frontend/src/components/common/ChatBox.jsx
+++ trivia-titans-frontend/src/components/common/ChatBox.jsx
@@ -1,86 +1,95 @@
-import {addDoc, collection, getFirestore, onSnapshot, orderBy, query} from 'firebase/firestore';
+import {addDoc, collection, getFirestore, onSnapshot, orderBy, query,serverTimestamp} from 'firebase/firestore';
 import firebaseClient from "./firebase";
 import React, {useContext, useEffect, useState} from "react";
 import {fetchMemberTeamData} from "./teamContext";
 import {AuthContext} from "./AuthContext";
+import { Button, TextField, List, ListItem, ListItemText, Typography, Container, Paper } from "@mui/material";
 
 const db = getFirestore(firebaseClient);
 
 const Chat = () => {
-    const [message, setMessage] = useState('' );
+    const [message, setMessage] = useState('');
     const [chat, setChat] = useState([]);
     const [isTeamPlayer,setIsTeamPlayer]=useState(false);
     const [name,setName]=useState('');
-   // const navigate = useNavigate();
-
     const [teamName,setTeamName] =useState(null);
     const currentUser = useContext(AuthContext)
+
     useEffect(() => {
         const getTeamPlayerData = async () => {
             if (currentUser) {
                 setName(currentUser.email);
-                console.log(currentUser);
                 const teamPlayerData = await fetchMemberTeamData(currentUser);
                 console.log(teamPlayerData);
                 if (teamPlayerData) {
                     setIsTeamPlayer(true);
-                    console.log(isTeamPlayer);
                     setTeamName(teamPlayerData.teamName);
                 }
-                return teamPlayerData;
             }
         }
-            getTeamPlayerData();
-
+        getTeamPlayerData();
     }, [currentUser]);
 
     useEffect(() => {
-        console.log(isTeamPlayer);
-        if(isTeamPlayer) {
+        if(isTeamPlayer && teamName) {
+
             const q = query(collection(db, teamName), orderBy('timestamp', 'desc'));
+            console.log(q);
             const unsubscribe = onSnapshot(q, (snapshot) => {
                 setChat(snapshot.docs.map(doc => doc.data()));
             });
             return () => unsubscribe();
         }
-    }, [db,teamName])
+    }, [db,teamName, isTeamPlayer])
 
     const onTextChange = (e) => {
         setMessage(e.target.value);
     }
+
     const onMessageSubmit = async (e) => {
         e.preventDefault();
-        if(message.length > 0) await addDoc(collection(db, teamName), { name, message });
-    }
-    const renderChat = () => {
-            return chat.map(({name, message}, idx) => (
-                <div key={idx}>
-                    <h4>
-                        {name}: <span>{message}</span>
-                    </h4>
-                </div>
-            ));
+        if(message.length > 0) {
+            await addDoc(collection(db, teamName), { name, message , timestamp: serverTimestamp() });
+            setMessage('');
         }
+    }
+
+    const renderChat = () => {
+        return chat.map(({name, message}, idx) => (
+            <ListItem key={idx}>
+                <ListItemText primary={name} secondary={message} />
+            </ListItem>
+        ));
+    }
+
     return (
-        <div>
+        <Container maxWidth="sm">
             {!isTeamPlayer && (
-                <div className="team-prompt">
-                    <p><strong> Please join a team or create one to chat with other team members, You can only join teams Upon
-                        invitation</strong></p>
-                </div>
+                <Typography variant="h6" color="error">
+                    Please join a team or create one to chat with other team members. You can only join teams upon invitation.
+                </Typography>
             )}
-            {isTeamPlayer && ( <div>
-            <h1>Chat Log</h1>
-            <form onSubmit={onMessageSubmit}>
-                <h3>Message</h3>
-                <input name="message" onChange={(e) => onTextChange(e)} value={message} id="outlined-multiline-static" variant="outlined" label="Message" />
-                <button>Send Message</button>
-            </form>
-            <div>
-                {renderChat()}
-            </div>
-        </div> )}
-        </div>
+            {isTeamPlayer && (
+                <Paper style={{ maxHeight: '70vh', overflow: 'auto' }}>
+                    <List>{renderChat()}</List>
+                    <form onSubmit={onMessageSubmit} style={{ position: 'sticky', bottom: 0, padding: '10px', background: 'white', borderTop: '1px solid #ddd' }}>
+                        <TextField
+                            name="message"
+                            onChange={onTextChange}
+                            value={message}
+                            id="outlined-multiline-static"
+                            variant="outlined"
+                            label="Message"
+                            fullWidth
+                        />
+                        <Button variant="contained" color="primary" type="submit" style={{ marginTop: '10px' }}>
+                            Send Message
+                        </Button>
+                    </form>
+                </Paper>
+            )}
+        </Container>
     )
 }
+
 export default Chat;
