b18c2a2e1f1629715526e5a7c92113a04e02a313
Rishi Vasa <rs310602@dal.ca>
2023-07-20 14:48:10 -0300

    Squashed commit of the following:
    commit 31f8677fa5cb09d968269880fa79531d11faf372
    Merge: 6297330 a818e62
    Author: Adithya Hadadi <ad368540@dal.ca>
    Date:   Thu Jul 20 12:37:23 2023 -0300
    
        Merge branch 'develop-adithya' into 'develop'
    
        Develop adithya
    
        See merge request vasa/csci5410-sdp-10!23
    
    commit a818e624867dfa4dce4a897e4d42b30bafe7f260
    Merge: 2975764 6297330
    Author: Adithya Hadadi <ad368540@dal.ca>
    Date:   Thu Jul 20 11:58:12 2023 -0300
    
        Merge branch 'develop' of https://git.cs.dal.ca/vasa/csci5410-sdp-10 into develop-adithya
    
    commit 2975764c4c8d5c445a561566cc68453a00429a3f
    Author: Adithya Hadadi <ad368540@dal.ca>
    Date:   Thu Jul 20 11:57:57 2023 -0300
    
         fixed minor bug in chat box
    
    commit ac37261e16078d2e2eab70a7222286a465faab2d
    Author: Adithya Hadadi <ad368540@dal.ca>
    Date:   Thu Jul 20 09:57:24 2023 -0300
    
         fixed delete functionality - the team now fully disbands in case admin removes themselves

--- trivia-titans-frontend/src/components/team/teamStats.jsx
+++ trivia-titans-frontend/src/components/team/teamStats.jsx
@@ -27,7 +27,7 @@ const TeamPage = () => {
     useEffect(() => {
         const getTeamPlayerData = async () => {
             if (currentUser) {
-                const teamPlayerData = await fetchMemberTeamData(currentUser);
+                const teamPlayerData = await fetchMemberTeamData(currentUser.email);
                 if (teamPlayerData) {
                     setTeamName(teamPlayerData.teamName);
                     setIsTeamPlayer(true);
@@ -100,19 +100,40 @@ const TeamPage = () => {
         setOpen(false);
     };
     const removeTeamMember = async function (playerEmail) {
-        //  e.preventDefault();
-        const jsonPayload2 = {
-            tableName: "teamMembers",
-            operation: "DELETE",
-            key: {
-                playerEmail: playerEmail
-            }
-        };
-        await invokeLambdaFunction('lambdaDynamoDBClient', jsonPayload2);
-        setTeamMembers(teamMembers.filter((team) => team.playerEmail !== playerEmail));
-        const teamPlayerData = await fetchMemberTeamData(currentUser);
-        if(!teamPlayerData)
+
+        const teamPlayerData = await fetchMemberTeamData(playerEmail);
+        if (!teamPlayerData)
             setIsTeamPlayer(false);
+        else {
+
+            // if admin removes themselves from the team, the whole team is disbanded.
+            if (teamPlayerData.teamPermission === 'ADMIN') {
+                console.log("player remove requested " +teamPlayerData.playerEmail);
+                for (let item of teamMembers) {
+                    const deleteUser = {
+                        tableName: "teamMembers",
+                        operation: "DELETE",
+                        key: {
+                            playerEmail: item.playerEmail.S
+                        }
+                    };
+                    await invokeLambdaFunction('Delete_DynamoDBClient', deleteUser);
+                    console.log("deleted player "+ item.playerEmail.S +" from team" +item.teamName.S);
+                    setTeamMembers(teamMembers.filter((team) => team.playerEmail !== item.playerEmail.S));
+                    setIsTeamPlayer(false);
+                }
+            } else if (teamPlayerData.teamPermission.S === 'MEMBER') {
+                const jsonPayload2 = {
+                    tableName: "teamMembers",
+                    operation: "DELETE",
+                    key: {
+                        playerEmail: playerEmail
+                    }
+                };
+                await invokeLambdaFunction('lambdaDynamoDBClient', jsonPayload2);
+                setTeamMembers(teamMembers.filter((team) => team.playerEmail !== playerEmail));
+            }
+        }
     }
     return (
         <div>
